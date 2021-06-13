3feea0822e65e7b06def780a90664b5dd4392836
Guilherme Magno <guilhermertfmagno@gmail.com>
2021-06-13 16:16:10 -0300

    Adiciona paginação customizada dos slides

--- js/script.js
+++ js/script.js
@@ -3,4 +3,4 @@ import SlideNav from './slide-nav.js'
 const slide = new SlideNav('.slide', '.slide-wrapper')
 slide.init()
 slide.addArrow('.prev', '.next')
-slide.addControls()
+slide.addControls('.custom-controls')
